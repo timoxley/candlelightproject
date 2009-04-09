d02ccce44728138b0648773aade5e408a6f85872
Douglas Catchpole <douglas@douglas-laptop.(none)>
2009-04-09 11:25:06 +1000

    Added test files.

---
+++ src/com/candlelightproject/lifemap/test/TestLoadGraph.java
@@ -0,0 +1,29 @@
+package com.candlelightproject.lifemap.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.candlelightproject.lifemap.graph.Graph;
+import com.candlelightproject.lifemap.graph.GraphLoader;
+
+public class TestLoadGraph {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File inFile = new File("test.xml");
+		GraphLoader gl = new GraphLoader();
+		try {
+			FileInputStream fis = new FileInputStream(inFile);
+			gl.load(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(Graph.saveString());
+		
+	}
+
+}
